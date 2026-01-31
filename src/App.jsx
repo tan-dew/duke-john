a4c5c7833f1ac20532aa701b12cff34e93424d12
tandew <tanmoy.dewanjee.16+github+tandew@gmail.com>
2026-01-31 07:14:48 +0600

    Prepare for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
 import Home from './pages/Home';
 import BookDetail from './pages/BookDetail';
